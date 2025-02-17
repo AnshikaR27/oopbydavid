3c213ee2177631ce185bd4265a5168429cf55b4f
AnshikaR27 <94916495+AnshikaR27@users.noreply.github.com>
2025-02-17 19:28:35 +0530

    Used a tuple instead; but did not seem to work for corner cases like Padma, since tuple does not support item assingment once initialized

--- student.py
+++ student.py
@@ -1,15 +1,14 @@
 def main():
-    name = get_name()
-    house = get_house()
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
 
-    print(f"{name} from {house}")
 
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
 
-def get_name():
-    return input("Name: ")
-
-def get_house():
-    return input("House: ")
-
+    return (name, house)
 if __name__ == "__main__":
     main()
